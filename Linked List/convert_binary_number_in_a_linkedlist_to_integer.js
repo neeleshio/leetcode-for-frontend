980c0c1303771d7e272e26f024107ac14c71ae26
Neelesh Shetty <neeleshshetty01@gmail.com>
2021-11-17 18:57:21 +0530

    getDecimalValue method

--- Linked List/convert_binary_number_in_a_linkedlist_to_integer.js
+++ Linked List/convert_binary_number_in_a_linkedlist_to_integer.js
@@ -5,10 +5,31 @@
  *     this.next = (next===undefined ? null : next)
  * }
  */
+/*https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/*/
 /**
  * @param {ListNode} head
  * @return {number}
  */
-var getDecimalValue = function (head) {
+const getDecimalValue = function (head) {
+    let result = 0
 
+    while (head) {
+        result = result * 2 + head.val
+        head = head.next
+    }
+    return result
 };
+
+//Recursive solution
+const getDecimalValueRecursively = function (head, dec = 0) {
+    if (!head) {
+        return dec
+    } else {
+        return getDecimalValueRecursively(head.next, dec * 2 + head.val)
+    }
+}
+
+let head = { val: 1, next: { val: 0, next: { val: 1, next: null } } }
+
+console.log(getDecimalValue(head))
+console.log(getDecimalValueRecursively(head))
